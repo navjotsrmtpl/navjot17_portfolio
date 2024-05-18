d3b757652a6f67d96cf61016404625c65107df21
navjotsrmtpl <navjotsingh950417@gmail.com>
2024-05-18 12:27:34 +0530

    amde responsive

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,17 +3,21 @@ import './Contact.css';
 
 function Contact() {
     let url = "";
-    const handlemailbox = () => {
-        url = "mailto:navjotsingh1795@gmail.com";
-
+    const handlebox = (value: string) => {
+        if (value === 'email') {
+            url = "mailto:navjotsingh1795@gmail.com";
+        }
+        if (value === 'mobile') {
+            url = 'tel:+918808294552'
+        }
         window.open(url, '_blank');
     }
     return (
         <section id="contact">
             <h1 className='heading'>Contact</h1>
             <div className='contact_section'>
-                <p className='pading_txt'>Email:<span className='font-wt' onClick={handlemailbox}>navjotsingh1795@gmail.com</span></p>
-                <p className='pading_txt'>Mobile:<span className='font-wt'>+918808294552</span></p>
+                <p className='pading_txt'>Email:<span className='font-wt' onClick={() => handlebox('email')}>navjotsingh1795@gmail.com</span></p>
+                <p className='pading_txt'>Mobile:<span className='font-wt' onClick={() => handlebox('mobile')}>+918808294552</span></p>
             </div>
         </section>
     )
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,98 @@
-import React from 'react'
+// Navbar.js
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
-import './navbar.css'
-// import logo from '../../assets/logo.gif'
+import './navbar.css';
 
-function Navbar() {
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <nav className='navbar'>
-            {/* <img src={logo} alt="logo" className='logo' /> */}
-            <p className="name">Navjot Singh</p>
-            <div className='destopMenu'>
-                <Link to="about" className="desktopMenuListItem" smooth={true} duration={500} offset={-60}>About</Link>
-                <Link to="experience" className="desktopMenuListItem" smooth={true} duration={500} offset={-60}>Experience</Link>
-                <Link to="projects" className="desktopMenuListItem" smooth={true} duration={500} offset={-60}>Projects</Link>
-                <Link to="skills" className="desktopMenuListItem" smooth={true} duration={500} offset={-60}>Skills</Link>
-                <Link to="contact" className="desktopMenuListItem" smooth={true} duration={500} offset={-60}>Contact</Link>
-            </div>
-            {/* <button className='desktopMenuBtn'>
-                Contact Me
-            </button> */}
-        </nav>
-    )
-}
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="navbar-logo">
+                    <h1>Navjot Singh</h1>
+                </div>
+                <div className="menu-icon" onClick={toggleNavbar}>
+                    <i className={isOpen ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={isOpen ? 'nav-menu active' : 'nav-menu'}>
 
-export default Navbar
+                    <li className="nav-item">
+                        <Link
+                            activeClass="active"
+                            to="about"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            onClick={toggleNavbar}
+                        >
+                            About
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            activeClass="active"
+                            to="experience"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            onClick={toggleNavbar}
+                        >
+                            Experience
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            activeClass="active"
+                            to="projects"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            onClick={toggleNavbar}
+                        >
+                            Projects
+                        </Link>
+
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            activeClass="active"
+                            to="skills"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            onClick={toggleNavbar}
+                        >
+                            Skills
+                        </Link>
+
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            activeClass="active"
+                            to="contact"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            onClick={toggleNavbar}
+                        >
+                            Contact
+                        </Link>
+
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
