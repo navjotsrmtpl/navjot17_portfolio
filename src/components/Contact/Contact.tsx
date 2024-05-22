97827826700f1aaa6b4919f9a599f03f20c31c3f
navjotsrmtpl <navjotsingh950417@gmail.com>
2024-05-22 19:48:51 +0530

    added dynamic styles

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
 
 function Contact() {
+
+    const [tooltip, setTooltip] = useState('Copy');
+
+
     let url = "";
     const handlebox = (value: string) => {
         if (value === 'email') {
@@ -12,15 +18,33 @@ function Contact() {
         }
         window.open(url, '_blank');
     }
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText('navjotsingh1795@gmail.com')
+            .then(() => {
+                setTooltip('Copied!');
+                setTimeout(() => setTooltip('Copy'), 2000); // Reset tooltip after 2 seconds
+            })
+            .catch(err => console.error('Failed to copy!', err));
+    };
+
+
     return (
         <section id="contact">
             <h1 className='heading'>Contact</h1>
             <div className='contact_section'>
-                <p className='pading_txt'>Email:<span className='font-wt' onClick={() => handlebox('email')}>navjotsingh1795@gmail.com</span></p>
+                <p className='pading_txt'>Email:<span className='font-wt' onClick={() => handlebox('email')}>navjotsingh1795@gmail.com</span>
+                    <button onClick={handleCopy} title={tooltip} style={{ position: 'relative' }} className='contact-btn'>
+                        <FontAwesomeIcon icon={faCopy} />
+                        <span style={{ visibility: tooltip === 'Copy' ? 'hidden' : 'visible' }} className="contact-style">
+                            {tooltip}
+                        </span>
+                    </button>
+                </p>
                 <p className='pading_txt'>Mobile:<span className='font-wt' onClick={() => handlebox('mobile')}>+918808294552</span></p>
             </div>
         </section>
     )
 }
 
-export default Contact
+export default Contact;
